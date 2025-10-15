fc06bd8b61bf9e162fbea9970ab04b1c3b50fc12
jun-0411 <jun411@snu.ac.kr>
2025-10-15 16:54:11 +0900

    웹사이트 기본 구조 (#2)

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>스누인턴</h1>
+      <div className={styles.menu}>
+        <Link to="/login" className={styles.link}>
+          <span>로그인</span>
+        </Link>
+        <Link to="/signup" className={styles.link}>
+          <span>회원가입</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <main className={styles.container}>
+      <span>Home Page</span>
+    </main>
+  );
+}
+export default Home;
