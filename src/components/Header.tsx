9a22f12cfe70e14d7d66b7245ad61fa265e33019
천준영 <c0912jy@gmail.com>
2025-10-17 15:27:11 +0900

    로그인 이후 네비게이션 내용 변경 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,39 @@
-import { Link } from 'react-router-dom';
+// src/components/Header.tsx
+import { Link, useNavigate } from 'react-router-dom';
+import useAuthStore from '../store/authStroe';
 import styles from './Header.module.css';
 
 function Header() {
+  const navigate = useNavigate();
+  const { isLoggedIn, username, logout } = useAuthStore();
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    logout();
+    navigate('/');
+  };
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>스누인턴</h1>
       <div className={styles.menu}>
-        <Link to="/login" className={styles.link}>
-          <span>로그인</span>
-        </Link>
-        <Link to="/signup" className={styles.link}>
-          <span>회원가입</span>
-        </Link>
+        {isLoggedIn ? (
+          <>
+            <span>{username}님</span>
+            <button type="button" onClick={handleLogout} className={styles.link}>
+              로그아웃
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/login" className={styles.link}>
+              <span>로그인</span>
+            </Link>
+            <Link to="/signup" className={styles.link}>
+              <span>회원가입</span>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
