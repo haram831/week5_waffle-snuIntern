e5e45f518f62e28494f97cf204f3f24e8d63685a
김하람 <gim222932@gmail.com>
2025-11-05 17:18:47 +0900

    컴포넌트 합치기

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,12 @@
+import JobContainer from '../components/JobContainer';
 import styles from './Home.module.css';
+
 
 function Home() {
   return (
     <main className={styles.container}>
       <span>Home Page</span>
+      <JobContainer />
     </main>
   );
 }
