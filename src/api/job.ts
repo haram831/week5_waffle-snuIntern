3b710bd9fd62dc38647b848689778cb0aaa6b6e2
김하람 <gim222932@gmail.com>
2025-11-05 17:13:57 +0900

    job 정보 api 요청

---
+++ src/api/job.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import type { JobListResponse } from "../@types/job.d.ts";
+import { encodeQueryParams } from "./encodeQueryParams";
+
+export const fetchJobList = async (
+    roles?: string[],
+    isActive?: boolean,
+    domains?: string[],
+    page?: number,
+    order?: 0 | 1
+): Promise<JobListResponse> => {
+    const baseUrl = 'https://api-internhasha.wafflestudio.com/api/post';
+    const queryString = encodeQueryParams({
+        params:{
+            roles: roles,
+            isActive: isActive,
+            domains: domains,
+            page: page,
+            order: order,
+        }
+    });
+    const url = `${baseUrl}?${queryString}`;
+
+    try {
+        const response = await axios.get(url, {
+            headers: { "Content-Type": "application/json" },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("공고 데이터를 가져오는 중 오류 발생:", error);
+    throw error;
+  }
+};
