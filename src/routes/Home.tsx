e0fd1d5b32b75693b036d101c6d230b6b23ba721
김하람 <gim222932@gmail.com>
2025-11-20 17:34:00 +0900

    관심 공고 component 구현

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,10 +1,12 @@
 import JobContainer from '../components/JobContainer';
+import MyBookMarks from '../components/MyPage/MyBookMarks';
 import styles from './Home.module.css';
 
 function Home() {
   return (
     <main className={styles.container}>
       <JobContainer />
+      <MyBookMarks />
     </main>
   );
 }
